e239d122ba96a44fcb83bbd6caa5c8e172b13da0
Nahuel <nawe7depb@gmail.com>
2020-04-30 20:22:27 -0300

    2.9*: The Phonebook Step4

--- Part 2/exercises2.6-2.10/src/App.js
+++ Part 2/exercises2.6-2.10/src/App.js
@@ -8,13 +8,14 @@ const Numbers = ({person}) => {
 
 const App = () => {
   const [ persons, setPersons ] = useState([
-    { 
-      name: 'Arto Hellas', 
-      number:'040-1234567'
-    }]) 
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }]) 
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
-
+  const [searchPerson, setSearchPerson] = useState('')
+  
   const addPerson = (event)=>{
     event.preventDefault()
     const personObject = {
@@ -29,30 +30,50 @@ const App = () => {
     else {window.alert(personObject.name + ' already exists')}
 
   }
+
+  const results = !searchPerson
+    ? persons 
+    : persons.filter(person =>
+        person.name.toLocaleLowerCase().includes(searchPerson.toLocaleLowerCase()))
+      
+
   const handleNameChange = (event) => {
     setNewName(event.target.value)
   }
   const handleNumberChange = (event) => {
     setNewNumber(event.target.value)
   }
+  const handleSearch = (event) => {
+    setSearchPerson(event.target.value)
+  }
   
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={addPerson}>
-        <div>
-          name: <input value={newName} onChange={handleNameChange}/>
-        </div>
-        <diV>
+      <div>
+        <form>
+          <div>
+            filter shown with <input value={searchPerson} onChange={handleSearch}/>
+          </div>
+        </form>
+      </div>
+      <div>
+        <h2>ad a new</h2>
+        <form onSubmit={addPerson}>
+          <div>
+            name: <input value={newName} onChange={handleNameChange}/>
+          </div>
+          <div>
           number: <input value={newNumber} onChange={handleNumberChange}/>
-        </diV>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
+          </div>
+          <div>
+            <button type="submit">add</button>
+          </div>
+        </form>
+      </div>
       <h2>Numbers</h2>
-      {persons.map(person => <Numbers key={person.name} person={person}/>)}
+      {results.map(person => <Numbers key={person.name} person={person}/>)}
     </div>
   )
 }
