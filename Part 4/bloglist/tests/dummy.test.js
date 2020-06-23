70e2587424cabda960abda140330b93161a98895
Nahuel <nawe7depb@gmail.com>
2020-06-23 17:37:15 -0300

    exercise 4.3

---
+++ Part 4/bloglist/tests/dummy.test.js
@@ -0,0 +1,9 @@
+const listHelper = require('../utils/list_helper').dummy
+
+test('dummy returns one', () => {
+    const blogs = []
+
+    const result = listHelper(blogs)
+
+    expect(result).toBe(1)
+})
